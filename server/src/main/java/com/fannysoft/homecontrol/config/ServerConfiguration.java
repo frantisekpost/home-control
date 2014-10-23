5bdf3bcd99f8a655c2c5d493764c84278f178276
Frantisek Post <frantisek.post@seznam.cz>
2014-10-23 15:52:51 +0200

    server broker configuration created in Config class annotated with @Configuration

---
+++ server/src/main/java/com/fannysoft/homecontrol/config/ServerConfiguration.java
@@ -0,0 +1,22 @@
+package com.fannysoft.homecontrol.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServerConfiguration {
+
+	@Bean
+	BrokerConfiguration brokerConfiguration() {
+		ImplicitBrokerConfiguration brokerConfiguration = new ImplicitBrokerConfiguration();
+		brokerConfiguration.setConsumeDestination(Destinations.SERVER_REGISTRATION_PATH);
+		brokerConfiguration.setPublishDestination(Destinations.AGENT_CONTROL_PATH);
+		brokerConfiguration.setHost("localhost");
+		brokerConfiguration.setPassword("password");
+		brokerConfiguration.setPort(1883);
+		brokerConfiguration.setUser("admin");
+		
+		return brokerConfiguration;
+	}
+	
+}
--- server/src/main/java/com/fannysoft/homecontrol/queue/AgentRegistrator.java
+++ server/src/main/java/com/fannysoft/homecontrol/queue/AgentRegistrator.java
@@ -1,24 +1,19 @@
 package com.fannysoft.homecontrol.queue;
 
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.fannysoft.homecontrol.config.Destinations;
-import com.fannysoft.homecontrol.config.ImplicitBrokerConfiguration;
+import com.fannysoft.homecontrol.config.BrokerConfiguration;
 
 @Component
 public class AgentRegistrator extends AbstractMessageTransporter implements InitializingBean {
 
+	@Autowired
+	private BrokerConfiguration brokerConfiguration;
+	
 	@Override
 	public void afterPropertiesSet() throws Exception {
-		ImplicitBrokerConfiguration brokerConfiguration = new ImplicitBrokerConfiguration();
-		brokerConfiguration.setConsumeDestination(Destinations.SERVER_REGISTRATION_PATH);
-		brokerConfiguration.setPublishDestination(Destinations.AGENT_CONTROL_PATH);
-		brokerConfiguration.setHost("localhost");
-		brokerConfiguration.setPassword("password");
-		brokerConfiguration.setPort(1883);
-		brokerConfiguration.setUser("admin");
-		
 		init(brokerConfiguration);
 	}
 
